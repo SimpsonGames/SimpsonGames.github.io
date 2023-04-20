02bc7ba8f28c9d518c59cd7ff8f02cc5f47a27f0
SimpsonGames <harrysimpson2001@gmail.com>
2023-04-20 16:20:31 +0100

    Added external links if valid

--- portfolio_import.js
+++ portfolio_import.js
@@ -10,11 +10,24 @@ fetch('portfolio.json')
       data.projects.forEach(project => {
         const projectElement = document.createElement('div');
         projectElement.classList.add('portfolio-panel');
-        projectElement.innerHTML = `
-          <img class="panel-image" src="${project.image}" alt="${project.title}">
-          <h3>${project.title} ${project.date}}</h3>
-          <p class="panel-desc">${project.description}</p>
-        `;
+
+        let projectInfo = `
+        <img class="panel-image" src="img/portfolio/panels/${project.image}" alt="${project.title}">
+        <h3>${project.title} (${project.date})</h3>
+        <p class="panel-desc">${project.description}</p>
+      `;
+
+        if (project.portfolioPage !== null || project.portfolioPage !== "#")
+        {
+          projectInfo += `<a href="${project.portfolioPage}"><button class="button">Read More</button></>`;
+        }
+
+        if (project.externalPage !== null || project.externalPage !== "#")
+        {
+          projectInfo += `<a href="${project.externalPage}" target="_blank"><button class="button">View Project Page</button></>`;
+        }
+      
+        projectElement.innerHTML = projectInfo;
         projectsContainer.appendChild(projectElement);
       });
     }
