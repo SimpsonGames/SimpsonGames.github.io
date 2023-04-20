a9c1bcaed529c4d616606427e6d2e0e44495f788
SimpsonGames <harrysimpson2001@gmail.com>
2023-04-20 15:21:30 +0100

    Put failcheck in script

--- portfolio_import.js
+++ portfolio_import.js
@@ -5,15 +5,22 @@ fetch('portfolio.json')
 
     const projectsContainer = document.querySelector('#portfolio-grid');
 
-    data.projects.forEach(project => {
-      const projectElement = document.createElement('div');
-      projectElement.classList.add('portfolio-panel');
-      projectElement.innerHTML = `
-        <img src="${project.image}" alt="${project.title}">
-        <h2>${project.title}</h2>
-        <p>${project.description}</p>
-      `;
-      projectsContainer.appendChild(projectElement);
-    });
+    if (projectsContainer)
+    {
+      data.projects.forEach(project => {
+        const projectElement = document.createElement('div');
+        projectElement.classList.add('portfolio-panel');
+        projectElement.innerHTML = `
+          <img src="${project.image}" alt="${project.title}">
+          <h2>${project.title}</h2>
+          <p>${project.description}</p>
+        `;
+        projectsContainer.appendChild(projectElement);
+      });
+    }
+    else
+    {
+      console.log('No element found with selector "#portfolio-grid"');
+    }
   });
 
