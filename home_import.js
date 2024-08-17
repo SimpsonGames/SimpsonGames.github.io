5695aa27f9a8626ff010364a57a9136fe8603046
SimpsonGames <harrysimpson2001@gmail.com>
2024-08-17 23:23:18 +0100

    Added featured games to home page

--- home_import.js
+++ home_import.js
@@ -22,9 +22,46 @@ fetch('about_me.json')
         {
             console.log('No element found with selector "#about-me-box"');
         }
+    });
 
-        //<h1 id="about-me-header"></h1>
-        //    <p id="about-me-body"></p>
-        //    <a href="/about_me"><button class="button">Read More</button></a>  
-        //    <hr></hr>
+fetch('portfolio.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const featuredContainer = document.querySelector('#featured-portfolio');
+
+        if (featuredContainer)
+        {
+            data.projects.forEach(project => {
+                if (project.featured)
+                {
+                    const featuredElement = document.createElement('div');
+                    featuredElement.classList.add('portfolio-panel');
+        
+                    let featuredInfo = `
+                    <img class="panel-image" src="img/portfolio/panels/${project.image}" alt="${project.title}">
+                    <h3>${project.title} (${project.date})</h3>
+                    <p class="panel-desc">${project.description}</p>
+                    `;
+        
+                    if (project.portfolioPage !== null && project.portfolioPage !== "#") 
+                    {
+                        featuredInfo += `<a href="portfolio/${project.portfolioPage}"><button class="button">Read More</button></>`;
+                    }
+        
+                    if (project.externalPage !== null && project.externalPage !== "#")
+                    {
+                        featuredInfo += `<a href="${project.externalPage}" target="_blank"><button class="button">View Project Page</button></>`;
+                    }
+              
+                    featuredElement.innerHTML = featuredInfo;
+                    featuredContainer.appendChild(featuredElement);
+                }
+            });
+        }
+        else
+        {
+            console.log('No element found with selector #featured-portfolio')
+        }
+
     });
