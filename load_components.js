d5c0eea334ce940b5b7dd33bae54224b1296931a
SimpsonGames <harrysimpson2001@gmail.com>
2024-08-18 11:24:20 +0100

    Test making header and footer separate objects

---
+++ load_components.js
@@ -0,0 +1,14 @@
+function loadComponent(path, targetElementID)
+{
+    fetch(path)
+        .then(response => response.text())
+        .then(html => {
+            const element = document.getElementById(targetElementID);
+            element.innerHTML = html;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent('header.html', '#header');
+    loadComponent('footer.html', '#footer');
+});
