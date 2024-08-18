0bfb0e467e53a9c99ae44be3020dd335f006d59a
SimpsonGames <harrysimpson2001@gmail.com>
2024-08-18 13:53:03 +0100

    Update import_portfolio_page.js

--- portfolio/import_portfolio_page.js
+++ portfolio/import_portfolio_page.js
@@ -1,6 +1,6 @@
 function importInfo(fileName)
 {
-    const filePath = '../portfolio/${fileName}.json';
+    const filePath = `${fileName}.json`;
 
     fetch(filePath)
         .then(response => response.json())
